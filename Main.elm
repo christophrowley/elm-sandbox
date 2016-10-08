e2f4e7a1b501879a984b296771a13d400fc94db7
Chris Rowley <christophrowley@gmail.com>
2016-10-08 18:46:07 +0100

    initial commit

---
+++ Main.elm
@@ -0,0 +1,58 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App as App
+
+
+-- model
+
+type alias Model =
+  { firstName: String
+  , lastName: String
+  , email: String }
+
+initModel : Model
+initModel =
+  { firstName = ""
+  , lastName = ""
+  , email = "" }
+
+
+-- update
+
+type Msg =
+    UpdateFirstName String
+  | UpdateLastName String
+  | UpdateEmail String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    UpdateFirstName val ->
+      { model
+      | firstName = val }
+    UpdateLastName val ->
+      { model
+      | lastName = val }
+    UpdateEmail val ->
+      { model
+      | email = val }
+
+
+-- view
+
+view : Model -> Html Msg
+view model =
+  div []
+      [ input [ type' "text", onInput UpdateFirstName ][]
+      , input [ type' "text", onInput UpdateLastName ][]
+      , input [ type' "text", onInput UpdateEmail ][]
+      ]
+
+main =
+  App.beginnerProgram
+    { model = initModel
+    , update = update
+    , view = view }
